bbc28420b5e31d9a4ddabee3001787a8fdd86e60
Gopi Manohar T <deathcoderx@gmail.com>
2019-11-26 02:19:22 +0530

    Create plusOne.cpp

---
+++ Solutions/plusOne.cpp
@@ -0,0 +1,18 @@
+ class Solution {
+ public:
+    vector<int> plusOne(vector<int>& nums) {
+        for(int i = nums.size()-1; i >= 0; i--)
+        {
+            if (nums[i] == 9)
+                nums[i] = 0;
+            else
+            {
+                nums[i]++;
+                return nums;
+            }
+        }
+        nums[0] = 1;
+        nums.push_back(0);
+        return nums;
+    }
+};
